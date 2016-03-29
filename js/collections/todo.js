56c4ac7441d5ad5745918882748445b3f60d2085
Carlito531 <charly.riviere@gmail.com>
2016-03-29 18:01:41 +0200

    Initial commit

---
+++ js/collections/todo.js
@@ -0,0 +1,44 @@
+// This particular pattern when seen at the top of files is used to create a namespace, i.e. 
+// a named object under which functions and variables can be created without unduly polluting the global object
+var app = app || {};
+
+// Todo Collection
+// ---------------
+
+// The collection of todos is backed by *localStorage* instead of a remote server
+var TodoList = Backbone.Collection.extend({
+    
+    // Reference to this collection's model
+    model: app.Todo,
+    
+    // Save all of the todo items under the '"todos-backbone"' namespace.
+    localStorage: new Backbone.LocalStorage('todos-backbone'),
+    
+    // Filter down the list of all todo items that are finished.
+    completed: function() {
+        return this.filter(function(todo)) {
+            return todo.get('completed');
+        });
+    },
+                                          
+    remaining: function() {
+        return this.whithout.apply(this, this.completed());
+    },
+        
+    // We keep the Todos in sequential order, despite being saved by unordered
+    // GUID in the database. This generates the next order number for new items
+    nextOrder: function() {
+        if (!this.length) {
+            return 1;
+        }
+      return this.last().get('order') + 1;  
+    },
+        
+    // Todos are sorted by their original insertion order
+    comparator: function(todo) {
+        return todo.get('order');
+    } 
+});
+
+// Create our globale collection of **Todos**
+app.Todos = new TodoList();
