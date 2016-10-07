4d027eb5caac9f719996b4c65696eed7f79aefa5
Carlito531 <charly.riviere@gmail.com>
2016-10-07 17:58:35 +0200

    correct issue

--- js/views/todos.js
+++ js/views/todos.js
@@ -29,7 +29,7 @@ app.TodoView = Backbone.View.extend({
     
     // Re-renders the titles of the todo item
     render: function () {
-        thes.$el.html( this.template( this.model.attributes ));
+        this.$el.html( this.template( this.model.attributes ));
         this.$input = this.$('edit');
         return this;
     },
